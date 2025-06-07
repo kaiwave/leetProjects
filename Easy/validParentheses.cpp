d663af0e22f0ae7a3d102445c1a2903fc587e334
kaiwave <malakaiharianto@gmail.com>
2025-06-07 11:32:05 +1000

    Solved

--- Easy/validParentheses.cpp
+++ Easy/validParentheses.cpp
@@ -3,14 +3,26 @@
 class Solution {
 public:
   bool isValid(string s) {
-    bool ans = false;
-
     stack<char> brackets;
 
     for (int i = 0; i < s.length(); i++) {
-      if (s[i] == "(" || s[i] == "[" || s[i] == "{") {
+      char c = s[i];
+      if (c == '(' || c == '[' || c == '{') {
+        brackets.push(c);
+      }
 
+      else {
+        if (brackets.empty()) return false;
+        char top = brackets.top();
+        if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+          brackets.pop();
+        }
+        else {
+          return false;
+        }
       }
     }
+
+    return brackets.empty();
   }
 };
