5b8be43a3fd436b22793205d2ce3f012391c4d27
kaiwave <malakaiharianto@gmail.com>
2025-06-10 19:24:12 +1000

    Binary stuff

---
+++ Easy/binarySum.cpp
@@ -0,0 +1,30 @@
+// Problem 67
+
+class Solution {
+public:
+  string addBinary(string a, string b) {
+    string result = "";
+    int carry = 0;
+    int i = a.size() - 1;
+    int j = b.size() - 1;
+
+    while (i >= 0 || j >= 0 || carry) {
+      int sum = carry;
+
+      if (i >= 0) {
+        sum += a[i] - '0';
+        i--;
+      }
+
+      if (j >= 0) {
+        sum += b[j] - '0';
+        j--;
+      }
+
+      carry = sum / 2;
+      result = char(sum % 2 + '0') + result;
+    }
+
+    return result;
+  }
+};
