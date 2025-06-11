cd781998c7bd32d5b039ab7d049aa620f7f3a7de
kaiwave <malakaiharianto@gmail.com>
2025-06-11 11:11:31 +1000

    Solved removing Duplicates List

--- Easy/removeDuplicatesList.cpp
+++ Easy/removeDuplicatesList.cpp
@@ -1,7 +1,33 @@
-//
-//  removeDuplicatesList.cpp
-//  leetProjects
-//
-//  Created by Malakai Harianto on 11/6/2025.
-//
+// Problem 83
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+  ListNode* deleteDuplicates(ListNode* head) {
+    ListNode* current = head;
+    
+    while (current != nullptr && current->next != nullptr) {
+      if (current->val == current->next->val) {
+
+        ListNode* duplicate = current->next;
+        current->next = current->next->next;
+        delete duplicate;
+      } else {
+        current = current->next;
+      }
+    }
+
+    return head;
+  }
+};
 
