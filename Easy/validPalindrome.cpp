7ae67b11d612d363237839d1eabd78536e82cc62
kaiwave <malakaiharianto@gmail.com>
2025-06-13 14:21:12 +1000

    more palindrome stuffs

---
+++ Easy/validPalindrome.cpp
@@ -0,0 +1,25 @@
+// Problem 125
+
+class Solution {
+public:
+  bool isPalindrome(string s) {
+    string cleaned = "";
+
+    for (char c : s) {
+      if (isalnum(c)) {
+        cleaned += tolower(c);
+      }
+    }
+
+    int left = 0, right = cleaned.length() - 1;
+    while (left < right) {
+      if (cleaned[left] != cleaned[right]) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+
+    return true;
+  }
+};
