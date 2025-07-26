c9e5fc62a4b835d093d6ef22957804da07ebc6f7
kaiwave <malakaiharianto@gmail.com>
2025-07-26 14:33:29 +1000

    optimise

--- Easy/longestCommonPrefix.rs
+++ Easy/longestCommonPrefix.rs
@@ -2,25 +2,19 @@
 
 impl Solution {
   pub fn longest_common_prefix(strs: Vec<String>) -> String {
-    if strs.is_empty() {
-      return String::new();
-    }
+    if strs.is_empty() { return String::new(); }
 
-    let strs_length = strs.len();
-    let first_word: Vec<char> = strs[0].chars().collect();
+    let strs_chars: Vec<Vec<char>> = strs.iter().map(|s| s.chars().collect()).collect();
+    let first_word = &strs_chars[0];
     let mut ans = String::new();
 
     for i in 0..first_word.len() {
       let letter = first_word[i];
-
-      for j in 0..strs_length {
-        let current_word: Vec<char> = strs[j].chars().collect();
-
-        if i >= current_word.len() || current_word[i] != letter {
+      for word in &strs_chars {
+        if i >= word.len() || word[i] != letter {
           return ans;
         }
       }
-
       ans.push(letter);
     }
 
