0b8c260ce73e4855baa841502038302e14b257d7
kaiwave <malakaiharianto@gmail.com>
2025-07-28 13:10:50 +1000

    valid par rust

---
+++ Easy/validParentheses.rs
@@ -0,0 +1,31 @@
+// 20 valid parentheses rusty
+
+impl Solution {
+  pub fn is_valid(s: String) -> bool {
+    let mut stack = Vec::new();
+    for c in s.chars() {
+      match c {
+        '(' | '{' | '[' => stack.push(c),
+        ')' => {
+          if stack.pop() != Some('(') {
+            return false;
+          }
+        }
+        '}' => {
+          if stack.pop() != Some('{') {
+            return false;
+          }
+        }
+        ']' => {
+          if stack.pop() != Some('[') {
+            return false;
+          }
+        }
+        _ => {
+          return false;
+        }
+      }
+    }
+    return stack.is_empty();
+  }
+}
