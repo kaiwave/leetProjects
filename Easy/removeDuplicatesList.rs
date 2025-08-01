ab2af659be33d8a4ec826a690cf5beb7566c42ef
kaiwave <malakaiharianto@gmail.com>
2025-08-01 17:27:46 +1000

    ?

--- Easy/removeDuplicatesList.rs
+++ Easy/removeDuplicatesList.rs
@@ -19,6 +19,21 @@
 
 impl Solution {
   pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        
+    let mut dummy = Box::new(ListNode { val: 0, next: None });
+    let mut tail = &mut dummy;
+    let mut current = head;
+    let mut prev_val: Option<i32> = None;
+
+    while let Some(mut node) = current {
+      current = node.next.take();
+      if prev_val.is_none() || prev_val.unwrap() != node.val {
+        tail.next = Some(node);
+        tail = tail.next.as_mut().unwrap();
+      }
+      prev_val = Some(tail.val);
+    }
+
+    tail.next = None;
+    dummy.next
   }
 }
